0db5fa0d644b6d9b9715b1b99b8780ef4c56f49e
victor <victoryil27oficial@gmail.com>
2020-11-11 01:36:40 +0100

    final

---
+++ src/Etapa.java
@@ -0,0 +1,13 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Etapa implements Runnable {
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+    @Override
+    public void run() {
+        System.out.printf("%s -> Comienza etapa\n",
+                LocalTime.now().format(dateTimeFormatter), Thread.currentThread().getName());
+
+    }
+}
